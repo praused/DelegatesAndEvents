ef41cfff4960dad0593427239a033d4ee98c0665
David Prause <praused@hotmail.com>
2017-09-05 16:23:36 -0400

    3 Handling Events, Instantiating Delegates and Handling Events

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -35,10 +35,22 @@ namespace DelegatesAndEvents
             //Console.WriteLine(finalHours);
 
             var worker = new Worker();
-
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            worker.DoWork(8, WorkType.GenerateReports);
 
             Console.Read();
 
+        }
+
+        static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Worker is done");
+        }
+
+        static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: " +e.Hours + " " + e.WorkType);
         }
 
         //static void DoWork(WorkPerformedHandler del)
--- DelegatesAndEvents/Worker.cs
+++ DelegatesAndEvents/Worker.cs
@@ -19,6 +19,7 @@ namespace DelegatesAndEvents
         {
             for (int i = 0; i < hours; i++)
             {
+                System.Threading.Thread.Sleep(1000);//pause each iteration by 1 second so we can watch it work. Never do this in real software.
                 //Call helper method to raise the event
                 OnWorkPerformed(i + 1, workType);
             }
