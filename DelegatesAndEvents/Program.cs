acb1a5daa01e5d96b94c1f7a2052ae4a9cb9570f
David Prause <praused@hotmail.com>
2017-09-05 12:20:07 -0400

    2 Creating Delegates, Events and Event Args, Creating a Custom Delegate

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+
+    //define delegate
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
     class Program
     {
         static void Main(string[] args)
         {
+            //create delegate instance wired to a handler method
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            ////invoke delegate to call its handler method
+            //del1(5, WorkType.Golf);
+            //del2(10, WorkType.GenerateReports);
+
+            //pass delegate instance to a method
+            //DoWork(del1);
+            DoWork(del1);
+
+            Console.Read();
+
         }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            //invoke passed delegate to call its handler method
+            del(5, WorkType.GoToMeetings);
+        }
+
+        //handler method for del1 delegate instance
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        }
+
+        //handler method for del2 delegate instance
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        }
+
     }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+
 }
